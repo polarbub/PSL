0e71ad74b4eef03029986c038df608de200314b9
polarbub <71790726+polarbub@users.noreply.github.com>
2022-03-04 19:59:08 -0600

    add first function! something to make argc and argv more useable

--- include/psl/psl.h
+++ include/psl/psl.h
@@ -2,11 +2,13 @@
 
 #include <chrono>
 #include <thread>
+#include <vector>
+#include <string>
 
 #define psl_sleep(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms))
 #define pls_getArrayLength(array) sizeof(array)/sizeof(array[0])
 #define psl_printVarContentsName(command) std::cout << #command << ": " << command << std::endl;
 
 namespace psl {
-
+    std::vector<std::string> argcvToStringVector(int argc, char** argv);
 }
--- src/psl.cpp
+++ src/psl.cpp
@@ -1,6 +1,16 @@
 #include <string>
 #include <vector>
-#include <iostream>
+//#include <iostream>
 
 #include "psl.h"
+
+std::vector<std::string> psl::argcvToStringVector(int argc, char** argv) {
+    std::vector<std::string> out;
+    int i = 1;
+    while(i <= argc) {
+        out.emplace_back(argv[i]);
+        i++;
+    }
+    return out;
+}
 
