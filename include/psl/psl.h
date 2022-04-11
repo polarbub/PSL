47c4f35a26e06ec843d7afd817649344358e45fb
polarbub <71790726+polarbub@users.noreply.github.com>
2022-04-11 17:23:12 -0500

    varios bugfixes

--- include/psl/psl.h
+++ include/psl/psl.h
@@ -5,10 +5,17 @@
 #include <vector>
 #include <string>
 
+//Sleep for ms
 #define psl_sleep(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms))
+//Get the length of a normal array
 #define pls_getArrayLength(array) sizeof(array)/sizeof(array[0])
+//Print the name and contents of a variable, very useful for debugging
 #define psl_printVarContentsName(command) std::cout << #command << ": " << command << std::endl;
+//This allows for helper functions
+//Any function that would normally be in main can be wrapped in this and a 'return -1;' at the bottom, and it will function the same as if it were in main.
+#define psl_helperFunctionRunner(INT) {int output = INT; if(output != -1) return output;}
 
 namespace psl {
-    std::vector<std::string> argcvToStringVector(int argc, char** argv);
+    //Convert an argc and argv to a vector of strings.
+    std::vector<std::string> argcvToStringVector(int &argc, char** argv);
 }
--- src/psl.cpp
+++ src/psl.cpp
@@ -4,10 +4,10 @@
 
 #include "psl.h"
 
-std::vector<std::string> psl::argcvToStringVector(int argc, char** argv) {
+std::vector<std::string> psl::argcvToStringVector(int &argc, char** argv) {
     std::vector<std::string> out;
     int i = 1;
-    while(i <= argc) {
+    while(i < argc) {
         out.emplace_back(argv[i]);
         i++;
     }
