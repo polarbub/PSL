a4a5833924f0821d9d26d8567292b60875575eaf
polarbub <71790726+polarbub@users.noreply.github.com>
2022-07-13 16:16:22 -0500

    Finished writing but not testing the threaddispatcher

--- include/psl/threadDispatcher.h
+++ include/psl/threadDispatcher.h
@@ -1,45 +1,135 @@
 #pragma once
 
-#include <queue>
-#include <mutex>
-#include <condition_variable>
-#include <future>
-#include <thread>
-#include <any>
+#include <csignal>
+#include "queue"
+#include "thread"
+#include "future"
+#include "iostream"
 
-namespace psl::Thread {
+#include "functional"
+
+namespace psl::thread {
+    class threadSafePrint {
+    public:
+        explicit threadSafePrint() {};
+
+        template <typename... T>
+        void print(const T&... items) {
+            const std::scoped_lock lock(stream_mutex);
+            (std::cout << ... << items);
+        }
+
+        template <typename... T>
+        void println(const T&... items) {
+            print(items..., '\n');
+        }
+
+    private:
+        //Keep it thread safe
+        mutable std::mutex stream_mutex = {};
+    };
+
+    namespace internal {
+        class EmptyException: public std::exception
+        {
+            virtual const char* what() const throw()
+            {
+                return "The queue was empty";
+            }
+        };
+    }
+
     template <typename T>
     class ThreadSafeQueue {
     public:
-        ThreadSafeQueue(void) : q(), m(), c() {}
+        internal::EmptyException emptyException;
+
+        ThreadSafeQueue(void) : q(), m() {}
         ~ThreadSafeQueue(void) {}
 
         // Add an element to the queue.
-        void queue(T t);
+        void queue(T t) {
+            std::scoped_lock lock(m);
+            q.push(t);
+        };
 
         // Get the "front"-element.
         // If the queue is empty return nullptr
         T grab();
 
+        bool isEmpty();
+
     private:
         std::queue<T> q;
         mutable std::mutex m;
-        std::condition_variable c;
     };
 
-    class ThreadDispatcher {
+    class PrioritizedThreadPool {
     public:
-        ThreadDispatcher (uint threads) {
+        PrioritizedThreadPool(uint threads);
+        ~PrioritizedThreadPool();
 
-        }
+        template <typename FunctionType, typename... ArgTypes, typename ReturnType = std::invoke_result_t<std::decay_t<FunctionType>, std::decay_t<ArgTypes>...>>
+        std::future<ReturnType> submit(const FunctionType& function, const ArgTypes &... args) {
+            std::future<ReturnType> returnHandle;
+            this->lowPrioirtyQueue->queue(createJob(function, args..., returnHandle));
+            return returnHandle;
+        };
+
+        template <typename FunctionType, typename... ArgTypes, typename ReturnType = std::invoke_result_t<std::decay_t<FunctionType>, std::decay_t<ArgTypes>...>>
+        std::future<ReturnType> submitHighPriority(const FunctionType& function, const ArgTypes &... args) {
+            std::future<ReturnType> returnHandle;
+            this->highPrioirtyQueue->queue(createJob(function, args..., returnHandle));
+
+            int i;
+            for(i = 0; i++ < this->threadCount;) {
+                if(!threadsInHighPriority[i]) {
+                    //This sends an interrupt to a thread, it doesn't kill it.
+                    pthread_kill(threadArray[i]->native_handle(), 10);
+                    threadsInHighPriority[i] = true;
+                }
+            }
+
+            return returnHandle;
+        };
+
     private:
-        std::thread* threadArray;
+        template <typename FunctionType, typename... ArgTypes, typename ReturnType = std::invoke_result_t<std::decay_t<FunctionType>, std::decay_t<ArgTypes>...>>
+        std::function<void()> createJob(const FunctionType& function, const ArgTypes &... args,  std::future<ReturnType>& returnHandle) {
+            std::function<ReturnType(ArgTypes...)> realFunction(function);
+            std::shared_ptr<std::promise<ReturnType>> promise = std::make_shared<std::promise<ReturnType>>();
+
+            std::function<void()> toCallInternal([args..., realFunction, promise, function] {
+                try {
+                    if constexpr (std::is_void_v<ReturnType>) {
+                        realFunction(args...);
+                        promise->set_value();
+                    } else {
+                        promise->set_value(realFunction(args...));
+                    }
+                } catch (...) {
+                    try {
+                        promise->set_exception(std::current_exception());
+                    } catch (...) {}
+                }
+            });
+            returnHandle = promise->get_future();
+            return toCallInternal;
+        };
+
+        bool shutdown = false;
+
+        std::thread** threadArray;
+        bool* threadsInHighPriority;
         uint threadCount;
-        ThreadSafeQueue<std::future<std::any>>* lowPrioirtyQueue;
-        ThreadSafeQueue<std::future<std::any>>* highPrioirtyQueue;
 
-        void threadHoldingFunction() {
+        ThreadSafeQueue<std::function<void()>>* lowPrioirtyQueue;
+        ThreadSafeQueue<std::function<void()>>* highPrioirtyQueue;
 
-        }
+        int poolsID = 0;
+        static std::vector<PrioritizedThreadPool*> *pools;
+
+        void threadHoldingFunction(int i);
+        static void highPriorityJobRunner(int i);
     };
 }
--- src/threadDispatcher.cpp
+++ src/threadDispatcher.cpp
@@ -1,20 +1,103 @@
 #include "psl.h"
 #include "threadDispatcher.h"
 
-template<typename T>
-void psl::Thread::ThreadSafeQueue<T>::queue(T t) {
-    std::lock_guard<std::mutex> lock(m);
-    q.push(t);
-    c.notify_one();
-}
+#include <mutex>
+#include "functional"
+#include "iostream"
+#include "signal.h"
 
 template<typename T>
-T psl::Thread::ThreadSafeQueue<T>::grab() {
-    std::unique_lock<std::mutex> lock(m);
+T psl::thread::ThreadSafeQueue<T>::grab() {
+    std::scoped_lock lock(m);
     if (q.empty()) {
-        return nullptr;
+        throw emptyException;
     }
     T val = q.front();
     q.pop();
     return val;
 }
+
+template<typename T>
+bool psl::thread::ThreadSafeQueue<T>::isEmpty() {
+    std::scoped_lock lock(m);
+    return q.empty();
+}
+
+psl::thread::PrioritizedThreadPool::PrioritizedThreadPool(uint threads) {
+    signal(10, PrioritizedThreadPool::highPriorityJobRunner);
+
+    if(this->pools == nullptr) {
+        pools = new std::vector<psl::thread::PrioritizedThreadPool*>;
+    }
+
+    lowPrioirtyQueue = new ThreadSafeQueue<std::function<void()>>;
+    highPrioirtyQueue = new ThreadSafeQueue<std::function<void()>>;
+
+    threadsInHighPriority = new bool[threads];
+
+    threadArray = new std::thread*[threads];
+    int i;
+    for(i = 0; i++ < threads;) {
+        std::cout << "made thread" << std::endl;
+        threadArray[i] = new std::thread(&PrioritizedThreadPool::threadHoldingFunction, this, i);
+    }
+
+    pools->push_back(this);
+}
+
+psl::thread::PrioritizedThreadPool::~PrioritizedThreadPool() {
+    pools->erase(pools->begin() + poolsID);
+    if(pools->empty()) {
+        delete pools;
+        pools = nullptr;
+    }
+
+    shutdown = true;
+    int i;
+    for(i = 0; i++ < threadCount;) {
+        while(!threadArray[i]->joinable()) psl_sleep(1);
+        threadArray[i]->join();
+
+        delete threadArray[i];
+    }
+    delete[] threadArray;
+    delete[] threadsInHighPriority;
+    delete lowPrioirtyQueue;
+    delete highPrioirtyQueue;
+}
+
+void psl::thread::PrioritizedThreadPool::threadHoldingFunction(int i) {
+    while(!this->shutdown) {
+        try {
+            (lowPrioirtyQueue->grab())();
+        } catch (internal::EmptyException& e) {
+            psl_sleep(1);
+        }
+    }
+}
+
+void psl::thread::PrioritizedThreadPool::highPriorityJobRunner(int i) {
+    PrioritizedThreadPool *This;
+    uint id = 0;
+
+    for(PrioritizedThreadPool* pool : *pools) {
+        for(i = 0; i++ < pool->threadCount;) {
+            if(pool->threadArray[i]->get_id() == std::this_thread::get_id()) {
+                id = i;
+                This = pool;
+            }
+        }
+    }
+
+    while(!This->shutdown) {
+        try {
+            (This->highPrioirtyQueue->grab())();
+        } catch (internal::EmptyException& e) {
+            break;
+        }
+    }
+
+    This->threadsInHighPriority[i] = false;
+}
+
+std::vector<psl::thread::PrioritizedThreadPool*> *psl::thread::PrioritizedThreadPool::pools = nullptr;
